c13d2a9d5b6a85f1dba86a3571c2f2261da72dc5
yang <yang@yangui-MacBookAir.local>
2025-09-17 13:08:30 +0900

    수정: Next.js Image 설정 및 카카오 OAuth 키 업데이트
    - 카카오 CDN 도메인(k.kakaocdn.net) 이미지 허용
    - REST API 키 및 어드민 키로 OAuth 설정 수정
    - 카카오 로그인 기능 정상 작동 확인

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
     env: {
         CUSTOM_KEY: process.env.CUSTOM_KEY,
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'k.kakaocdn.net',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'k.kakaocdn.net',
+                port: '',
+                pathname: '/**',
+            }
+        ],
+    },
 };
 
 module.exports = nextConfig;
